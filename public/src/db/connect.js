86c8e458558fdd8783fd7df0e7af8c656bfa32ad
garvazsof <garvazsof@gmail.com>
2022-12-29 16:39:56 -0600

    modified variables names in code to read them in uppercase

--- public/src/db/connect.js
+++ public/src/db/connect.js
@@ -1,18 +1,44 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = __importDefault(require("mongoose"));
-const config_1 = __importDefault(require("config"));
+const dotenv = __importStar(require("dotenv"));
+//import config from "config";
 const logger_1 = __importDefault(require("../logger"));
 function connect() {
     var _a;
-    let dbUri = config_1.default.get("dbUri");
+    //let dbUri = config.get<string>("dbUri");
+    dotenv.config();
     logger_1.default.info("Config");
-    //let dbUri = process.env.dbUri || "mongodb+srv://IAMMUSER:<pwd>@cluster0.hf1dq.mongodb.net/IAMM?retryWrites=true&w=majority";
-    const password = (_a = process.env.mongoPassword) !== null && _a !== void 0 ? _a : "";
+    let dbUri = process.env.DBURI || "mongodb+srv://IAMMUSER:<pwd>@cluster0.hf1dq.mongodb.net/IAMM?retryWrites=true&w=majority";
+    const password = (_a = process.env.MONGOPASSWORD) !== null && _a !== void 0 ? _a : "";
     logger_1.default.info(dbUri);
+    logger_1.default.info("pwd: " + password);
     dbUri = dbUri.replace("<pwd>", password);
     return mongoose_1.default
         .connect(dbUri)
--- src/db/connect.ts
+++ src/db/connect.ts
@@ -1,18 +1,20 @@
 import mongoose, { ConnectOptions } from "mongoose";
 import * as dotenv from "dotenv";
 
-import config from "config";
+//import config from "config";
 import log from "../logger";
 
 
 function connect() {
     
-    let dbUri = config.get<string>("dbUri");
+    //let dbUri = config.get<string>("dbUri");
+    dotenv.config();
     log.info("Config");
-    //let dbUri = process.env.dbUri || "mongodb+srv://IAMMUSER:<pwd>@cluster0.hf1dq.mongodb.net/IAMM?retryWrites=true&w=majority";
-    const password = process.env.mongoPassword ?? "";
+    let dbUri = process.env.DBURI || "mongodb+srv://IAMMUSER:<pwd>@cluster0.hf1dq.mongodb.net/IAMM?retryWrites=true&w=majority";
+    const password = process.env.MONGOPASSWORD ?? "";
 
     log.info(dbUri);
+    log.info("pwd: " + password);
 
     dbUri = dbUri.replace("<pwd>", password);    
 
